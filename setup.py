c1761744ff4f8140309114919fc015826d3c9956
Bruno Clermont <bruno.clermont@gmail.com>
2012-10-11 13:31:44 +0200

    Revert "be more specific on templates"
    This reverts commit 9023a942a7a23b29b593345c800df3f849c10393.

--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ setup(
     packages = find_packages(),
     package_data={
         'tastydocs' : [
-            'templates/tastydocs/*.html',
+            '*.*',
         ]
     },
-    zip_safe=False,
     long_description="Automagic web documentation for tastypie REST APIs."
 )
 
